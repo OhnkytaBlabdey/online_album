423bb215eff1a6642c391e49e65823d5fc9f049a
NeWive <40617586+NeWive@users.noreply.github.com>
2018-12-21 17:21:17 +0800

    Add files via upload

---
+++ src/main/java/onlineAlbum/util/JavaConnectMySQL.java
@@ -0,0 +1,11 @@
+package onlineAlbum.util;
+
+public class JavaConnectMySQL {
+    String userName = null;
+    String password = null;
+    String driverName = null;
+    String url = null;
+    public JavaConnectMySQL(){
+        
+    }
+}
