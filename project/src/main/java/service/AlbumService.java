c379be5e156e9557496bd500415d4bb35b617dfe
OhnkytaBlabdey <1263189143@qq.com>
2019-01-06 19:50:05 +0800

    album service

--- project/src/main/java/service/AlbumService.java
+++ project/src/main/java/service/AlbumService.java
@@ -1,7 +1,14 @@
 package service;
 
+import dao.AlbumDao;
+import po.Album;
+
 public class AlbumService {
+	private AlbumDao albumDao=new AlbumDao();
 	
+	public Album findAlbum(String name) {
+		return albumDao.findByAlbumName(name);
+	}
 public static void main(String[] args) {
 	
 }
