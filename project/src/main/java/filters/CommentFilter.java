31d2a2e98f9feaa736070124a258df76b563aa90
OhnkytaBlabdey <1263189143@qq.com>
2019-01-09 22:44:36 +0800

    ..

---
+++ project/src/main/java/filters/CommentFilter.java
@@ -0,0 +1,68 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.UserService;
+
+/**
+ * Servlet Filter implementation class CommentFilter
+ */
+@WebFilter(urlPatterns="/comment")
+public class CommentFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public CommentFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		String username = (String) ((HttpServletRequest)request).getSession().getAttribute("username");
+		if(username!=null) { // validate
+			;
+			// check if user name exists in database
+			UserService userService=new UserService();
+			if(userService.findByUserNameService(username) != null) {
+				chain.doFilter(request, response);
+			}
+		}
+		else {
+			HttpServletResponse resp=(HttpServletResponse) response;
+			resp.sendRedirect("./Login.jsp");
+			return;
+		}
+
+		// pass the request along the filter chain
+		//chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
--- project/src/main/java/servlets/CommentServlet.java
+++ project/src/main/java/servlets/CommentServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,15 @@ public class CommentServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter writer=response.getWriter();
+		String comment= (String) request.getParameter("input_comment");
+//		String username=null;
+//		String albumid=null;
+		writer.println("comment  "+comment);
+		writer.append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
