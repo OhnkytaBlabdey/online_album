1e6887e0ba426bf34ba18f1e7db19a862e93bdd8
OhnkytaBlabdey <1263189143@qq.com>
2019-01-10 21:52:12 +0800

    encode

--- project/src/main/java/utility/ImageUtil.java
+++ project/src/main/java/utility/ImageUtil.java
@@ -17,7 +17,7 @@ public class ImageUtil {
 	public static final String GIF = "gif";
 
 	/**
-	 * 在Servlet中调用该方法, JSP页面中img标签的src指向该Servlet, 则会显示图片
+	 *   Servlet  , JSP   img srcָ   Servlet,    
 	 * 
 	 * @param response
 	 * @param path
@@ -25,15 +25,15 @@ public class ImageUtil {
 	 */
 	public static void showImage(HttpServletResponse response,OutputStream out, String path, boolean isResponseClose) {
 		try {
-//			ServletOutputStream outStream = response.getOutputStream();// 得到向客户端输出二进制数据的对象
-			FileInputStream fis = new FileInputStream(path); // 以byte流的方式打开文件
+//			ServletOutputStream outStream = response.getOutputStream();//
+			FileInputStream fis = new FileInputStream(path); // 
 			byte data[] = new byte[4096];
 			while (fis.read(data) > 0) {
 				out.write(data);
 			}
 			fis.close();
-			response.setContentType("image/*"); // 设置返回的文件类型
-			out.write(data); // 输出数据
+			response.setContentType("image/*"); //   
+			out.write(data); //        
 			if (isResponseClose) {
 				out.close();
 			}
@@ -43,7 +43,7 @@ public class ImageUtil {
 	}
 
 	/**
-	 * 在Servlet中调用该方法, JSP页面中img标签的src指向该Servlet, 则会显示图片
+	 *   Servlet   , JSP   img srcָ   Servlet,   
 	 * 
 	 * @param response
 	 * @param data
@@ -51,11 +51,11 @@ public class ImageUtil {
 	 */
 	public static void showImage(HttpServletResponse response, byte[] data, boolean isResponseClose) {
 		try {
-			ServletOutputStream outStream = response.getOutputStream();// 得到向客户端输出二进制数据的对象
-			// 读数据
+			ServletOutputStream outStream = response.getOutputStream();//    
+			//       
 			outStream.write(data);
-			response.setContentType("image/*"); // 设置返回的文件类型
-			outStream.write(data); // 输出数据
+			response.setContentType("image/*"); //  
+			outStream.write(data); //        
 			if (isResponseClose) {
 				outStream.close();
 			}
@@ -65,9 +65,9 @@ public class ImageUtil {
 	}
 
 	/**
-	 * 在Servlet中调用该方法, JSP页面中img标签的src指向该Servlet, 则会显示图片
+	 *  Servlet , JSP img  Servlet  
 	 * 
-	 * @param response
+	 * 
 	 * @param image
 	 * @param imgType
 	 * @param isResponseClose
